bbf7da7e6e287c2dd70b93e7e24a9a21f1e72fd9
Diannedejong <150512@csgaugustinus.nl>
2020-04-15 08:03:49 +0000

    j

--- public/PHP/opdracht_4.php
+++ public/PHP/opdracht_4.php
@@ -9,6 +9,7 @@ $priem=array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
 echo '<h1>Het vierde element van de array $priem is: '.$priem[3].' </h1>';
 echo $priem[count($priem)-1];
 
+
 /****************************
 EINDE VAN JOUW PHPCODE
 ****************************/
